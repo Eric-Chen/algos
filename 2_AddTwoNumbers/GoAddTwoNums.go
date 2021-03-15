ffc9a360505914a7767eb14e8a4e496fcf140590
eric <ct@kuaihuoyun.com>
2021-03-15 18:58:51 +0800

    add 2 numbers

---
+++ 2_AddTwoNumbers/GoAddTwoNums.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+func main() {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 0,
+				Next: &ListNode{
+					Val: 1,
+				},
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 3,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 0,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}
+	l := addTwo(l1, l2)
+	for ; l != nil; l = l.Next {
+		fmt.Println(l.Val)
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwo(l1 *ListNode, l2 *ListNode) *ListNode {
+	var l *ListNode = &ListNode{}
+	r := l
+	for {
+		v1, v2 := 0, 0
+		if l1 != nil {
+			v1 = l1.Val
+		}
+		if l2 != nil {
+			v2 = l2.Val
+		}
+		v := v1 + v2
+		if v > 9 {
+			l.Val = l.Val + (v - 10)
+			l.Next = &ListNode{
+				Val: 1,
+			}
+		} else {
+			l.Val = l.Val + v
+		}
+
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+
+		if l1 == nil && l2 == nil {
+			break
+		} else {
+			if l.Next == nil {
+				l.Next = &ListNode{}
+			}
+			l = l.Next
+		}
+	}
+
+	return r
+}
