f00dddfb12d7b2c2cf50306f4ed46199eedca326
eric <ct@kuaihuoyun.com>
2021-03-15 20:06:12 +0800

    ...

---
+++ 2_AddTwoNumbers/add_two_numbers/src/add_tn.rs
@@ -0,0 +1,15 @@
+mod add_tn {
+    pub struct ListNode {
+        pub val: i32,
+        pub next: *ListNode,
+    }
+
+    pub fn add_two_numbers(l1: *ListNode, l2: *ListNode) -> *ListNode {
+        let l: *ListNode = &ListNode{};
+        let r: *ListNode = l;
+
+        while(true) {
+            
+        }
+    }    
+}
