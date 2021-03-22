16bd4d4b57b8414bd34ca2600d0cca3486f68106
eric <ct@kuaihuoyun.com>
2021-03-22 11:42:38 +0800

    algo-3

---
+++ 3_Longest_Substring_Without_Repeating_Characters/go_impl/Longest.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+func lengthOfLongestSubstring(s string) int {
+	l := len(s)
+	for i := 0; i < l; i++ {
+
+	}
+}
---
+++ 3_Longest_Substring_Without_Repeating_Characters/rs_impl/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+
+}
