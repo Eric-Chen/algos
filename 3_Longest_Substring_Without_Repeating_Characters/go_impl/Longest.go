f1ccef7f4d67e4856bfbc82b8e1c15ea75d23c1f
eric <ct@kuaihuoyun.com>
2021-03-31 15:00:45 +0800

    3 go update

--- 3_Longest_Substring_Without_Repeating_Characters/go_impl/Longest.go
+++ 3_Longest_Substring_Without_Repeating_Characters/go_impl/Longest.go
@@ -1,12 +1,45 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(lengthOfLongestSubstring("abcab"))
 }
 
 func lengthOfLongestSubstring(s string) int {
-	l := len(s)
-	for i := 0; i < l; i++ {
+	i, j := 0, 0
+	if len(s) == 1 {
+		return 1
+	}
+	maxLen := 0
+	jump := false
+	for i < len(s) {
+		jump = false
+		m := make(map[string]int)
+		for j = i + 1; j < len(s); j++ {
+			l, ok := m[string(s[j])]
+			if ok {
+				if l > i {
+					i = l
+					jump = true
+					break
+				}
+			} else {
+				m[string(s[j])] = j
+			}
+			maxLen = max(j-i, maxLen)
+		}
+		if !jump {
+			i++
+		}
+	}
+	return maxLen
+}
 
+func max(i, j int) int {
+	if i > j {
+		return i
+	} else {
+		return j
 	}
 }
